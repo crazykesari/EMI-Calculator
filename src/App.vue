942a1a04ecc92c6515defdb03c66018730d63634
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2021-09-27 01:46:01 +0530

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <EMICalculator />
+    <router-view />
+  </div>
+</template>
+<script>
+import EMICalculator from "@/components/EMICalculator";
+
+export default {
+  name: "App",
+  components: {
+    EMICalculator,
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: yellow;
+  margin: 0px;
+}
